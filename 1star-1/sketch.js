3e85facceb4b0148516ef8243f0f5e5b6bb36bfb
Jeremy Southern <jezzd11@outlook.com>
2026-02-12 14:44:31 +0000

    done first exersize

--- 1star-1/sketch.js
+++ 1star-1/sketch.js
@@ -1,10 +1,42 @@
-const userNames = new set();
+let textInput;
+let button;
+let output;
+const userNames = new Set();
+
+
 function setup()
 {
     createCanvas(400,400)
+    textInput = createInput();
+    button = createButton('input')
+    textInput.position(170,202)
+    button.position(360,202)
+    button.mousePressed(inputName)
+    
 }
-
 function draw()
 {
+    background(255);
+    textSize(23)
+    text('username:',22,100)
+    text(output,100,200)
+    
+}
 
+function inputName()
+{
+    text(output,100,200)
+    if (userNames.has(textInput.value()))
+    {
+        output = 'username in use'
+    }
+    else if (textInput.value() === '')
+    {
+        output = 'username cannot be empty'
+    }
+    else
+    {
+        userNames.add(textInput.value())
+        output = (`hello ${textInput.value()}`)
+    }
 }
