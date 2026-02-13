9b871257b6db35a7789e7e639307f01a563b4b0c
Jeremy Southern <jezzd11@outlook.com>
2026-02-13 00:09:11 +0000

    third one mostly done just need to add order change fully

--- 1star-2/sketch.js
+++ 1star-2/sketch.js
@@ -6,6 +6,7 @@ let pressedCount = 0;
 function setup()
 {
     createCanvas(400,400);
+    fillMaps();
 
 }
 
@@ -18,15 +19,21 @@ function mousePressed()
 {
     background(255)
     pressedCount++;
+    fill(fillCols.get(pressedCount))
+    strokeWeight(10)
+
+    stroke(strokeCols.get(pressedCount))
     for (let i =0; i < pressedCount; i ++)
     {
-        ellipse(random(400),random(400),50)
+        ellipse(random(400),random(400),random(50))
     }
 }
 
-function fillSets()
+function fillMaps()
 {
-    fillCols.set('1',100)
-    strokeCols.set('1',255)
-
+    for (let i = 0; i < 100; i++)
+    {
+        fillCols.set(i,random(255))
+        strokeCols.set(i,random(255))
+    }
 }
--- 2star-1/sketch.js
+++ 2star-1/sketch.js
@@ -2,27 +2,41 @@
 const columns = [];
 const NUM_SHAPES = 5;
 let ascending = true;
+let sortText;
+let orderText;
+let button;
 
-function setup() {
+function setup() 
+{
     createCanvas(500, 200);
     randomShapes();
+    textSize(15)
+    button = createButton('order')
+    button.mousePressed(orderChange)
 }
 
-function draw() {
+function draw() 
+{
+    
     background(255);
-    for (let i = 0; i < columns.length; i++) {
+    text((sortText +', ' +orderText),10,20)
+    for (let i = 0; i < columns.length; i++) 
+    {  
         const x = width / NUM_SHAPES * i;
         const y = height - columns[i].height;
         columns[i].draw(x, y);
     }
+    
 }
 
 
 /**
  * Populates the array with random Data objects
  */
-function randomShapes() {
-    for (let i = 0; i < NUM_SHAPES; i++) {
+function randomShapes() 
+{
+    for (let i = 0; i < NUM_SHAPES; i++) 
+    {
         const dataWidth = floor(random(1, 5.1)) * 20;
         const dataHeight = floor(random(1, 10.1)) * 20;
         const dataColour = color(random(255), random(255), random(255));
@@ -33,7 +47,8 @@ function randomShapes() {
 /**
  * Represents a piece of data e.g. a column in a bar chart.
  */
-class Data {
+class Data 
+{
     width;
     height;
     colour;
@@ -59,5 +74,95 @@ class Data {
         rectMode(CORNER);
         fill(this.colour);
         rect(x, y, this.width, this.height);
+    }
+}
+
+function keyPressed()
+{
+
+    if (key === 'w' )
+    {
+        sortText = 'width'
+        for(let i = 0; i < columns.length; i++)
+        {
+            for(let j = i + 1; j < columns.length; j++)
+            {
+                if (ascending === true)
+                {
+                    if (columns[i].width < columns[j].width)
+                    {
+                        let temp = columns[i];
+                        columns[i] = columns[j]
+                        columns[j] = temp;
+                    } 
+                }
+
+                else 
+                {
+                    if (columns[i].width > columns[j].width)
+                    {
+                        let temp = columns[i];
+                        columns[i] = columns[j]
+                        columns[j] = temp;
+                    } 
+                }
+                           
+            }
+        }
+    }
+
+    if (key === 'h')
+    {
+        sortText = 'height'
+        for(let i = 0; i < columns.length; i++)
+        {
+            for(let j = i + 1; j < columns.length; j++)
+            {
+                if (columns[i].height < columns[j].height)
+                {
+                    let temp = columns[i];
+                    columns[i] = columns[j]
+                    columns[j] = temp;
+
+                }            
+            }
+        }
+    }
+
+    if (key === 'c')
+    {
+        sortText = 'colour'
+        for(let i = 0; i < columns.length; i++)
+        {
+            const colI = columns[i].colour;
+            const colsI = (int(red(colI))+int(green(colI))+int(blue(colI)))
+            for(let j = i + 1; j < columns.length; j++)
+            {
+                const colJ = columns[j].colour;
+                const colsJ = (int(red(colJ))+int(green(colJ))+int(blue(colJ)))
+                if (colsI < colsJ)
+                {
+                    let temp = columns[i];
+                    columns[i] = columns[j]
+                    columns[j] = temp;
+
+                }            
+            }
+
+        }
+    }
+}
+
+function orderChange()
+{
+    if (ascending === true)
+    {
+        ascending = false;
+        orderText = 'decending'
+    }
+    else
+    {
+        ascending = true;
+        orderText = 'acending'
     }
 }
